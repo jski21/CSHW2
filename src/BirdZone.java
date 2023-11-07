acfc0cf8ffa8b24280083e7476ea0053da1abf46
Jason Albrecht <56939800+jski21@users.noreply.github.com>
2023-11-07 16:13:15 -0500

    Heres to comittin
    Test

---
+++ src/BirdZone.java
@@ -0,0 +1,66 @@
+
+public class BirdZone implements Zoneable {
+    // Constructor
+    public BirdZone(String name, Coord2D location) {
+        // Empty body
+    }
+    
+    // Implement all methods from Zoneable with empty bodies
+    public String getName() {
+        // Empty body
+        return "";
+    }
+    
+    public Coord2D getLocation() {
+        // Empty body
+        return null;
+    }
+
+    @Override
+    public int petsInZone() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'petsInZone'");
+    }
+
+    @Override
+    public Petable heaviestPet() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'heaviestPet'");
+    }
+
+    @Override
+    public int inHumanYears(String petName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inHumanYears'");
+    }
+
+    @Override
+    public Zoneable restockPetFood(String foodName, int foodAmt) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'restockPetFood'");
+    }
+
+    @Override
+    public Zoneable feedZone() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'feedZone'");
+    }
+
+    @Override
+    public Petable getPet(String petName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPet'");
+    }
+
+    @Override
+    public String getPantryLabel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPantryLabel'");
+    }
+
+    @Override
+    public String closestPet(int x, int y) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'closestPet'");
+    }
+}
---
+++ src/Cat.java
@@ -0,0 +1,45 @@
+
+public class Cat implements Petable {
+    // Constructor
+    public Cat(String name, Coord2D location) {
+        // Empty body
+    }
+    
+    // Implement all methods from Petable with empty bodies
+    public void moveTo(Coord2D newLocation) {
+        // Empty body
+    }
+    
+    public String makeSound() {
+        // Empty body
+        return "";
+    }
+    
+    public String getName() {
+        // Empty body
+        return "";
+    }
+    
+    public Coord2D getLocation() {
+        // Empty body
+        return null;
+    }
+
+    @Override
+    public int getWeightInOz() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWeightInOz'");
+    }
+
+    @Override
+    public int getAge() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAge'");
+    }
+
+    @Override
+    public int eats(String foodLabel) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eats'");
+    }
+}
